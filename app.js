9bda8db6708197b2b55e384f52c43618cc56b1ed
andika <zoneandika@gmail.com>
2022-08-19 00:54:58 +0700

    menyambungkan dengan framework tailwindcss dan menambahkan struktur di index

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ const express = require('express');
 const port = 3000;
 
 const app = express();
+app.set('view engine', 'ejs');
+
+// built-in middleware
+app.use(express.static('public'));
+
+// application level middleware
+app.get('/', (req,res) => {
+    res.render('index');
+})
+
+app.get('/account', (req,res) => {
+    res.render('account');
+});
+
+app.use('/', (req,res) => {
+    res.status(404);
+    res.send('Halaman eror');
+});
+
+app.listen(port, () => {
+    console.log(`app berjalan di localhost:${port}`);
+});
